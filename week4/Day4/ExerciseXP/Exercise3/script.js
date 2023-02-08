ff2b4f8e34daf014b4cd3859cab4aaad82b37d91
gitelmanmarcelo <marcelo.gitelman@gmail.com>
2023-02-08 21:27:38 +0200

    fix

--- week4/Day4/ExerciseXP/Exercise3/script.js
+++ week4/Day4/ExerciseXP/Exercise3/script.js
@@ -6,7 +6,7 @@ const users = { user1: 18273, user2: 92833, user3: 90315 }
 // Excepted output: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
 // FYI : The number is their ID number.
 
-let usersArray = Object.entries(users).map(([key, value]) => [key, value]);
+let usersArray = Object.entries(users);
 
 // Modify the outcome of part 1, by multipling the user’s ID by 2.
 // Excepted output: [ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
