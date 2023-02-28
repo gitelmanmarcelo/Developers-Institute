03248a98c3ac46773c9ebd1b4a693d9212223932
gitelmanmarcelo <marcelo.gitelman@gmail.com>
2023-02-28 10:24:11 +0200

    week7 day2 v3

--- Week7/Day2/DailyChallenge/server.js
+++ Week7/Day2/DailyChallenge/server.js
@@ -2,10 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use('/',express.static(__dirname+'/public'));
+
 app.get('/aboutme/:hobby', (req, res) => {
     const hb = req.params.hobby;
     if (/^[a-zA-Z]+$/.test(hb))
-        res.end(hb);
+        res.send(hb);
     else    
         res.sendStatus(404);
 });
@@ -18,8 +23,8 @@ app.get('/form', (req,res) => {
     res.sendFile(__dirname + '/public/form.html');
 }) 
 
-app.get('/formdata', (req, res) => {
-    res.end(req.query.mail + ' sent you a message: ' + req.query.message);
+app.post('/formdata', (req, res) => {
+    res.send(req.body.mail + ' sent you a message: ' + req.body.message);
 })
 
 app.listen(5000,() => {
